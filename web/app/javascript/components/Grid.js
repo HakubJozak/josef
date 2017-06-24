bce5394180a27e8853b45f385cb7b264bf9f0fda
Jakub Hozak <jakub.hozak@gmail.com>
2017-06-24 22:24:42 +0200

    * grid working

--- web/app/javascript/components/Grid.js
+++ web/app/javascript/components/Grid.js
@@ -8,11 +8,13 @@ class Grid extends React.Component {
     const row = Array(10).fill(null);
     const squares = Array(10).fill(null);
 
-    squares.forEach(function(row,y) {
-      row.forEach(function(square,x) {
-        row[x] = { x: x, y: y };
-      })
-    })             
+    for (var y =0; y < 10; y++) {
+      squares[y] = new Array(10);
+
+      for (var x =0; x < 10; x++) {
+        squares[y][x] = new Object();
+      }
+    }
 
     squares[3][3].robot = true;
     
@@ -31,22 +33,23 @@ class Grid extends React.Component {
     this.setState({ message: phrase })
   }
 
-  renderRow (row) {
-    const squaresJsx = row.map(function(square,i) {
-       const key = `${square.x}-${square.y}`
-       return <Square x={square.x} y={square.y} robot={square.robot} key={key} />;
+  renderRow (row,y) {
+    const squaresJsx = row.map(function(square,x) {
+       const key = `${x}.${y}`
+       return <Square x={x} y={y} robot={square.robot} key={key} />;
     });
 
+
     return (
-        <div className='j-grid-row'>
+        <div className='j-grid-row' key={y}>
           {squaresJsx}
         </div>
     )
   }
 
   render () {
-    const squaresJsx = this.state.squares.map((row,i) => {
-      return this.renderRow(row)
+    const squaresJsx = this.state.squares.map((row,y) => {
+      return this.renderRow(row,y)
     });
 
     return (
