1ba764b817140e85758578f72e2c2a91d61281ea
Jakub Hozak <jakub.hozak@gmail.com>
2017-09-08 19:35:00 +0200

    * variable grid

--- web/app/javascript/components/Playground.js
+++ web/app/javascript/components/Playground.js
@@ -5,28 +5,31 @@ import Konva from 'konva';
 export default class Playground extends React.Component {
   constructor(props) {
     super(props);
+    this.height = 500;
+    this.width  = 500;
+    this.squareSize = 50;
   }
 
   componentDidMount () {
       // first we need to create a stage
       this.stage = new Konva.Stage({
         container: 'konva-playground',   // id of container <div>
-        width: 500,
-        height: 500
+        width: this.width,
+        height: this.height
       });
 
       // then create layer
       this.grid = new Konva.Layer();
 
-      for (var i =1; i < 10; i++) {
+      for (var i = this.squareSize; i < this.width; i += this.squareSize) {
            var horizontal = new Konva.Line({
-             points: [ 0, i * 50, 500, i * 50 ],
+             points: [ 0, i, this.height, i],
              stroke: 'black',
              strokeWidth: 1
            });
 
            var vertical = new Konva.Line({
-             points: [ i * 50, 0, i * 50, 500 ],
+             points: [ i, 0, i, this.height ],
              stroke: 'black',
              strokeWidth: 1
            });
